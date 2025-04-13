7948c145d55b4d924c1aeeaaa171653acd7ada34
Khushijc <151053815+Khushijc@users.noreply.github.com>
2025-04-13 20:32:51 +0530

    Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,8 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Khushii!!</h1>
         <p className={styles.description}>
-          I'm a second-year student pursuing a B.tech degree in computer engineering at RCPIT,shirpur . I am passionate about web development and have a keen interest in exploring new technologies. I am always eager to learn and grow in the field of technology.
+          I'm a second-year student pursuing a B.tech degree in computer engineering at RCPIT,shirpur . 
+          I am passionate about web development and have a keen interest in exploring new technologies. 
         </p>
         <a href="mailto:khushijc2106@gmail.com" className={styles.contactBtn}>
           Contact Me
